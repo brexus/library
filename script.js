f95d2692332aa2c161bc2aa14c22392307624f22
brexus <92671269+brexus@users.noreply.github.com>
2023-07-09 12:15:37 +0200

    add function deleteBooksListeners and add it inside function startLibrary

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ function clearLibraryFromPage() {
     booksContainer.innerHTML = "";
 }
 
+function deleteBooksListeners() {
+    const btnsDelete = document.querySelectorAll(".btn-book-delete");
+
+    btnsDelete.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            reloadLibrary();
+        });
+    });
+}
+
+function reloadLibrary() {
+    clearLibraryFromPage();
+    addLibraryToPage();
+}
+
 function addLibraryToPage() {
     clearLibraryFromPage();
 
@@ -51,7 +67,7 @@ function addLibraryToPage() {
         }
         const bookIcons = document.createElement("div");
         bookIcons.classList.add("book-icons");
-        bookIcons.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>pencil-outline</title><path d='M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z' /></svg><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>delete-outline</title><path d='M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z' /></svg><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>star-outline</title><path d='M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z' /></svg>";
+        bookIcons.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>pencil-outline</title><path d='M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z' /></svg><svg class='btn-book-delete' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>delete-outline</title><path d='M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z' /></svg><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>star-outline</title><path d='M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z' /></svg>";
 
         bookCart.appendChild(bookTitle);
         bookCart.appendChild(bookAuthor);
@@ -60,7 +76,7 @@ function addLibraryToPage() {
         bookCart.appendChild(bookIcons);
         booksContainer.appendChild(bookCart);
     });
-
+    deleteBooksListeners();
 }
 
 function addBookListener() {
@@ -101,7 +117,7 @@ function addFormWindow() {
             formWindow.className = "active";
     });
 
-    addBookListener()
+    addBookListener();
 }
 
 function statusReadListener() {
@@ -124,6 +140,7 @@ function startLibrary() {
     addFormWindow();
     addLibraryToPage();
     statusReadListener();
+    deleteBooksListeners();
 }
 
 
